3d3704e7970e8ff147bf760b98561454d140e8a5
Samaneh <samaneh2010gh@gmail.com>
2021-07-16 13:40:43 -0400

    9 tests passing for testing about page help popup window

--- navbar/about_page.py
+++ navbar/about_page.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.support.color import Color
 from selenium.webdriver.common.keys import Keys
 import unittest
 
@@ -39,8 +40,46 @@ class AboutPage(unittest.TestCase):
         expected = "HELP: ABOUT"
         self.assertEqual(expected, actual)
 
-    def tearDown(self) -> None:
-        self.about_driver.quit()
+    def test_about_page_help_header_color(self):
+        self.about_driver.find_element_by_xpath(
+            "/html/body/app-root/app-nav-bar/nav/ul/li[2]/app-help-button/a").click()
+        color = self.about_driver.find_element_by_xpath(
+            "/html/body/ngb-modal-window/div/div/app-about-help/app-help/div/label[1]").value_of_css_property("color")
+        actual = Color.from_string(color).hex
+        expected = "#3fa9f5"
+        self.assertEqual(expected, actual)
+
+    def test_about_page_help_header_font(self):
+        self.about_driver.find_element_by_xpath(
+            "/html/body/app-root/app-nav-bar/nav/ul/li[2]/app-help-button/a").click()
+        actual = self.about_driver.find_element_by_xpath(
+            "/html/body/ngb-modal-window/div/div/app-about-help/app-help/div/label[1]").value_of_css_property("font-family")
+        expected = "days-font"
+        self.assertEqual(expected, actual)
+
+    def test_about_page_help_paragraph_text(self):
+
+        self.about_driver.find_element_by_xpath("/html/body/app-root/app-nav-bar/nav/ul/li[2]/app-help-button/a").click()
+        actual = self.about_driver.find_element_by_xpath(
+            "/html/body/ngb-modal-window/div/div/app-about-help/app-help/div/label[2]/p").text
+        expected = "Find us on Facebook by visiting the links on this page."
+        self.assertEqual(expected, actual)
+
+    def test_about_page_help_paragraph_text_color(self):
+        self.about_driver.find_element_by_xpath(
+            "/html/body/app-root/app-nav-bar/nav/ul/li[2]/app-help-button/a").click()
+        color = self.about_driver.find_element_by_xpath(
+            "/html/body/ngb-modal-window/div/div/app-about-help/app-help/div/label[2]/p").value_of_css_property("color")
+        actual = Color.from_string(color).hex
+        expected = "#ff9e13"
+        self.assertEqual(expected, actual)
+
+    def test_about_page_help_submit_button(self):
+        self.about_driver.find_element_by_xpath(
+            "/html/body/app-root/app-nav-bar/nav/ul/li[2]/app-help-button/a").click()
+        actual = self.about_driver.find_element_by_class_name("alert-box-button").text
+        expected = "CLOSE"
+        self.assertEqual(expected, actual)
 
 
 if __name__ == '__main__':
